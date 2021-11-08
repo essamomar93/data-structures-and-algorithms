03cf096bf59f8593b702ea5a09ac2160fdc73f64
essam omar <essam.93@hotmail.com>
2021-11-08 22:46:22 +0400

    1

---
+++ javascript/401-code-challenges/linked-list-insertions/__tests__/linkList.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const LinkedList = require('../lib/linked-list');
+
+describe('linked-list test', () => {
+  it("Can successfully instantiate an empty linked list", () => {
+    const linkList = new LinkedList();
+    expect(linkList).toBeDefined();
+    expect(linkList.head).toBeNull();
+  });
+
+  it("Can properly insert into the linked list", () => {
+    const ll = new LinkedList();
+    ll.insert("a");
+    expect(ll.head.value).toEqual("a");
+    expect(ll.head.next).toBeNull();
+  });
+
+  it("The head property will properly point to the first node in the linked list", () => {
+    const ll = new LinkedList();
+    ll.insert("a");
+    ll.insert("b");
+    expect(ll.head.value).toEqual("a");
+    expect(ll.head.next.value).toEqual("b");
+  });
+
+  it("Can properly insert multiple nodes into the linked list", () => {
+    const ll = new LinkedList();
+    ll.insert("c");
+    ll.insert("b");
+    ll.insert("a");
+    expect(ll.head.value).toBeDefined();
+    expect(ll.head.next.value).toBeDefined();
+    expect(ll.head.next.next.value).toBeDefined();
+  });
+
+  it("Will return true when finding a value within the linked list that exists", () => {
+    const ll = new LinkedList();
+    ll.insert("c");
+    ll.insert("b");
+    ll.insert("a");
+    expect(ll.includes("a")).toBeTruthy();
+    expect(ll.includes("b")).toBeTruthy();
+    expect(ll.includes("c")).toBeTruthy();
+  });
+
+  it("Will return false when searching for a value in the linked list that does not exist", () => {
+    const ll = new LinkedList();
+    ll.insert("a");
+    expect(ll.includes("b")).toBeFalsy();
+  });
+
+  it("Can properly return a collection of all the values that exist in the linked list", () => {
+    const ll = new LinkedList();
+    ll.insert("a");
+    ll.insert("b");
+    ll.insert("c");
+    expect(ll.toString()).toBeDefined();
+    expect(ll.toString().length).toBeGreaterThan(0);
+    expect(ll.toString()).toEqual("{ a } -> { b } -> { c } -> X");
+  });
+})
