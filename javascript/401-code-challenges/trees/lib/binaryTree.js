e8f9f57f1475ac557594a4e9a7df7c58f199f04b
essam omar <essam.93@hotmail.com>
2021-11-21 19:03:17 +0400

    lab16

--- javascript/401-code-challenges/trees/lib/binaryTree.js
+++ javascript/401-code-challenges/trees/lib/binaryTree.js
@@ -39,14 +39,16 @@ class BinaryTree {
     _walk(this.root);
     return arrayOfReadNodes;
   }
-
   maximumValue() {
-    let max = this.root;
-    while (max.right) {
-      max = max.right;
-    }
-    return max.value;
+    let max = this.root.value;
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      max = node.value > max ? node.value : max;
+    };
+    traverse(this.root);
 
+    return max;
   }
 
 }
